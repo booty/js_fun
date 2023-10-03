0f0c55b2b3d5d20b76f19425a69785ee5e6dc6b4
John Rose <johnedmundrose@gmail.com>
2023-10-03 18:19:15 -0400

    New app, ahoy

---
+++ toy_widgets/src/App.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <h1>Widgets</h1>
+  <div class="simple-widget-box simple-widget">
+    <p>Here's a little box for you.</p>
+    <p>We could put something here.</p>
+  </div>
+</template>
+
